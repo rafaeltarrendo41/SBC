94ed6a5f61996e807dc9a61d63e0ea61bdcb1b08
Francisca Cunha <57408771+francisca9422@users.noreply.github.com>
2021-04-26 21:08:03 +0100

    Create BC_todos_casos_possiveis.pl

---
+++ BC_todos_casos_possiveis.pl
@@ -0,0 +1,483 @@
+Ficheiro BC:
+
+%regras base de conhecimento
+
+if 'Fastfood' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil1'
+
+if 'Fastfood' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil2'
+
+if 'Fastfood' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil3'
+
+if 'Fastfood' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil4'
+
+if 'Fastfood' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil5'
+
+if 'Fastfood' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil6'
+
+if 'Fastfood' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil7'
+
+if 'Fastfood' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil8'
+
+if 'Fastfood' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil9'
+
+if 'Fastfood' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil10'
+
+if 'Fastfood' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil11'
+
+if 'Fastfood' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil12'
+
+if 'Fastfood' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil13'
+
+if 'Fastfood' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil14'
+
+if 'Fastfood' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil15'
+
+if 'Fastfood' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil16'
+
+if 'Fastfood' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil17'
+
+if 'Fastfood' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil18'
+
+if 'Fastfood' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil19'
+
+if 'Fastfood' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil20'
+
+if 'Fastfood' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil21'
+
+if 'Fastfood' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil22'
+
+if 'Fastfood' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil23'
+
+if 'Fastfood' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil24'
+
+if 'Fastfood' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil25'
+
+if 'Fastfood' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil26'
+
+if 'Fastfood' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil27'
+
+if 'Fastfood' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil28'
+
+if 'Fastfood' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil29'
+
+if 'Fastfood' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil30'
+
+if 'Pizza' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil31'
+
+if 'Pizza' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil32'
+
+if 'Pizza' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil33'
+
+if 'Pizza' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil34'
+
+if 'Pizza' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil35'
+
+if 'Pizza' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil36'
+
+if 'Pizza' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil37'
+
+if 'Pizza' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil38'
+
+if 'Pizza' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil39'
+
+if 'Pizza' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil40'
+
+if 'Pizza' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil41'
+
+if 'Pizza' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil42'
+
+if 'Pizza' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil43'
+
+if 'Pizza' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil44'
+
+if 'Pizza' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil45'
+
+if 'Pizza' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil46'
+
+if 'Pizza' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil47'
+
+if 'Pizza' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil48'
+
+if 'Pizza' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil49'
+
+if 'Pizza' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil50'
+
+if 'Pizza' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil51'
+
+if 'Pizza' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil52'
+
+if 'Pizza' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil53'
+
+if 'Pizza' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil54'
+
+if 'Pizza' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil55'
+
+if 'Pizza' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil56'
+
+if 'Pizza' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil57'
+
+if 'Pizza' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil58'
+
+if 'Pizza' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil59'
+
+if 'Pizza' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil60'
+
+if 'ComidaPortuguesa' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil61'
+
+if 'ComidaPortuguesa' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil62'
+
+if 'ComidaPortuguesa' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil63'
+
+if 'ComidaPortuguesa' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil64'
+
+if 'ComidaPortuguesa' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil65'
+
+if 'ComidaPortuguesa' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil66'
+
+if 'ComidaPortuguesa' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil67'
+
+if 'ComidaPortuguesa' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil68'
+
+if 'ComidaPortuguesa' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil69'
+
+if 'ComidaPortuguesa' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil70'
+
+if 'ComidaPortuguesa' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil71'
+
+if 'ComidaPortuguesa' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil72'
+
+if 'ComidaPortuguesa' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil73'
+
+if 'ComidaPortuguesa' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil74'
+
+if 'ComidaPortuguesa' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil75'
+
+if 'ComidaPortuguesa' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil76'
+
+if 'ComidaPortuguesa' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil77'
+
+if 'ComidaPortuguesa' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil78'
+
+if 'ComidaPortuguesa' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil79'
+
+if 'ComidaPortuguesa' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil80'
+
+if 'ComidaPortuguesa' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil81'
+
+if 'ComidaPortuguesa' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil82'
+
+if 'ComidaPortuguesa' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil83'
+
+if 'ComidaPortuguesa' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil84'
+
+if 'ComidaPortuguesa' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil85'
+
+if 'ComidaPortuguesa' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil86'
+
+if 'ComidaPortuguesa' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil87'
+
+if 'ComidaPortuguesa' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil88'
+
+if 'ComidaPortuguesa' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil89'
+
+if 'ComidaPortuguesa' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil90'
+
+if 'Sushi' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil91'
+
+if 'Sushi' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil92'
+
+if 'Sushi' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil93'
+
+if 'Sushi' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil94'
+
+if 'Sushi' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil95'
+
+if 'Sushi' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil96'
+
+if 'Sushi' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil97'
+
+if 'Sushi' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil98'
+
+if 'Sushi' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil99'
+
+if 'Sushi' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil100'
+
+if 'Sushi' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil101'
+
+if 'Sushi' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil102'
+
+if 'Sushi' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil103'
+
+if 'Sushi' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil104'
+
+if 'Sushi' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil105'
+
+if 'Sushi' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil106'
+
+if 'Sushi' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil107'
+
+if 'Sushi' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil108'
+
+if 'Sushi' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil109'
+
+if 'Sushi' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil110'
+
+if 'Sushi' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil111'
+
+if 'Sushi' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil112'
+
+if 'Sushi' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil113'
+
+if 'Sushi' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil114'
+
+if 'Sushi' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil115'
+
+if 'Sushi' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil116'
+
+if 'Sushi' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil117'
+
+if 'Sushi' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil118'
+
+if 'Sushi' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil119'
+
+if 'Sushi' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil120'
+
+if 'Chines' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil121'
+
+if 'Chines' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil122'
+
+if 'Chines' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil123'
+
+if 'Chines' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil124'
+
+if 'Chines' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil125'
+
+if 'Chines' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil126'
+
+if 'Chines' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil127'
+
+if 'Chines' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil128'
+
+if 'Chines' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil129'
+
+if 'Chines' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil130'
+
+if 'Chines' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil131'
+
+if 'Chines' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil132'
+
+if 'Chines' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil133'
+
+if 'Chines' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil134'
+
+if 'Chines' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil135'
+
+if 'Chines' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil136'
+
+if 'Chines' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil137'
+
+if 'Chines' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil138'
+
+if 'Chines' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil139'
+
+if 'Chines' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil140'
+
+if 'Chines' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil141'
+
+if 'Chines' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil142'
+
+if 'Chines' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil143'
+
+if 'Chines' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil144'
+
+if 'Chines' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil145'
+
+if 'Chines' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil146'
+
+if 'Chines' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil147'
+
+if 'Chines' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil148'
+
+if 'Chines' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil149'
+
+if 'Chines' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil150'
+
+if 'Vegetariano' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil151'
+
+if 'Vegetariano' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil152'
+
+if 'Vegetariano' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil153'
+
+if 'Vegetariano' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil154'
+
+if 'Vegetariano' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil155'
+
+if 'Vegetariano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil156'
+
+if 'Vegetariano' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil157'
+
+if 'Vegetariano' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil158'
+
+if 'Vegetariano' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil159'
+
+if 'Vegetariano' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil160'
+
+if 'Vegetariano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil161'
+
+if 'Vegetariano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil162'
+
+if 'Vegetariano' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil163'
+
+if 'Vegetariano' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil164'
+
+if 'Vegetariano' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil165'
+
+if 'Vegetariano' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil166'
+
+if 'Vegetariano' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil167'
+
+if 'Vegetariano' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil168'
+
+if 'Vegetariano' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil169'
+
+if 'Vegetariano' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil170'
+
+if 'Vegetariano' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil171'
+
+if 'Vegetariano' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil172'
+
+if 'Vegetariano' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil173'
+
+if 'Vegetariano' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil174'
+
+if 'Vegetariano' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil175'
+
+if 'Vegetariano' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil176'
+
+if 'Vegetariano' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil177'
+
+if 'Vegetariano' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil178'
+
+if 'Vegetariano' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil179'
+
+if 'Vegetariano' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil180'
+
+if 'Indiano' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil181'
+
+if 'Indiano' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil182'
+
+if 'Indiano' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil183'
+
+if 'Indiano' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil184'
+
+if 'Indiano' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil185'
+
+if 'Indiano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil186'
+
+if 'Indiano' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil187'
+
+if 'Indiano' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil188'
+
+if 'Indiano' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil189'
+
+if 'Indiano' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil190'
+
+if 'Indiano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil191'
+
+if 'Indiano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil192'
+
+if 'Indiano' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil193'
+
+if 'Indiano' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil194'
+
+if 'Indiano' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil195'
+
+if 'Indiano' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil196'
+
+if 'Indiano' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil197'
+
+if 'Indiano' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil198'
+
+if 'Indiano' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil199'
+
+if 'Indiano' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil200'
+
+if 'Indiano' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil201'
+
+if 'Indiano' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil202'
+
+if 'Indiano' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil203'
+
+if 'Indiano' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil204'
+
+if 'Indiano' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil205'
+
+if 'Indiano' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil206'
+
+if 'Indiano' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil207'
+
+if 'Indiano' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil208'
+
+if 'Indiano' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil209'
+
+if 'Indiano' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil210'
+
+if 'Italiano' and 'Carnivoro(carne)' and 'Takeaway' and 'Barato' then 'perfil211'
+
+if 'Italiano' and 'Carnivoro(carne)' and 'Takeaway' and 'Caro' then 'perfil212'
+
+if 'Italiano' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Barato' then 'perfil213'
+
+if 'Italiano' and 'Carnivoro(carne)' and 'HomeDelivery' and 'Caro' then 'perfil1214'
+
+if 'Italiano' and 'Carnivoro(carne)' and 'DriveIn' and 'Barato' then 'perfil215'
+
+if 'Italiano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil216'
+
+if 'Italiano' and 'Carnivoro(peixe)' and 'Takeaway' and 'Barato' then 'perfil217'
+
+if 'Italiano' and 'Carnivoro(peixe)' and 'Takeaway' and 'Caro' then 'perfil218'
+
+if 'Italiano' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Barato' then 'perfil219'
+
+if 'Italiano' and 'Carnivoro(peixe)' and 'HomeDelivery' and 'Caro' then 'perfil220'
+
+if 'Italiano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Barato' then 'perfil221'
+
+if 'Italiano' and 'Carnivoro(peixe)' and 'DriveIn' and 'Caro' then 'perfil222'
+
+if 'Italiano' and 'Vegetariano' and 'Takeaway' and 'Barato' then 'perfil223'
+
+if 'Italiano' and 'Vegetariano' and 'Takeaway' and 'Caro' then 'perfil224'
+
+if 'Italiano' and 'Vegetariano' and 'HomeDelivery' and 'Barato' then 'perfil225'
+
+if 'Italiano' and 'Vegetariano' and 'HomeDelivery' and 'Caro' then 'perfil226'
+
+if 'Italiano' and 'Vegetariano' and 'DriveIn' and 'Barato' then 'perfil227'
+
+if 'Italiano' and 'Vegetariano' and 'DriveIn' and 'Caro' then 'perfil228'
+
+if 'Italiano' and 'Vegan' and 'Takeaway' and 'Barato' then 'perfil229'
+
+if 'Italiano' and 'Vegan' and 'Takeaway' and 'Caro' then 'perfil230'
+
+if 'Italiano' and 'Vegan' and 'HomeDelivery' and 'Barato' then 'perfil231'
+
+if 'Italiano' and 'Vegan' and 'HomeDelivery' and 'Caro' then 'perfil232'
+
+if 'Italiano' and 'Vegan' and 'DriveIn' and 'Barato' then 'perfil233'
+
+if 'Italiano' and 'Vegan' and 'DriveIn' and 'Caro' then 'perfil234'
+
+if 'Italiano' and 'Crudivoro' and 'Takeaway' and 'Barato' then 'perfil235'
+
+if 'Italiano' and 'Crudivoro' and 'Takeaway' and 'Caro' then 'perfil236'
+
+if 'Italiano' and 'Crudivoro' and 'HomeDelivery' and 'Barato' then 'perfil237'
+
+if 'Italiano' and 'Crudivoro' and 'HomeDelivery' and 'Caro' then 'perfil238'
+
+if 'Italiano' and 'Crudivoro' and 'DriveIn' and 'Barato' then 'perfil239'
+
+if 'Italiano' and 'Crudivoro' and 'DriveIn' and 'Caro' then 'perfil240'
